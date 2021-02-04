48024fec065da7568417ba2481ef99509e4b1510
Macias <henry.macias@pnnl.gov>
2021-02-04 14:01:14 -0800

    updating cdk

--- Stack-Finder-CDK/lib/stack-finder-webservice.ts
+++ Stack-Finder-CDK/lib/stack-finder-webservice.ts
@@ -22,31 +22,31 @@ export class WebService extends Construct {
         const repository = Repository.fromRepositoryName(this, 'repository-stack-finder', props.repository);
 
         const vpc = ec2.Vpc.fromVpcAttributes(this, 'vpc-stack-finder', {
-            vpcId: 'vpc-0f94c45582fddf8d3',
+            vpcId: 'vpc-',
             availabilityZones: ['us-west-2a', 'us-west-2b'],
-            privateSubnetIds: ['subnet-03566bf4fb38625cf', 'subnet-0a78040a961aa7479'],
-            publicSubnetIds: ['subnet-0f0e8eaf4df836b78', 'subnet-02a1099a610ac9ef3'],
-            publicSubnetRouteTableIds: ['rtb-08ca0aa7e8e0e3b15', 'rtb-0707ce68c98d44cdc'],
-            privateSubnetRouteTableIds: ['rtb-064b11fc98c0a6e12', 'rtb-03ade6ab8a2de13c7']
+            privateSubnetIds: ['subnet-', 'subnet-'],
+            publicSubnetIds: ['subnet-', 'subnet-'],
+            publicSubnetRouteTableIds: ['rtb-', 'rtb-'],
+            privateSubnetRouteTableIds: ['rtb-', 'rtb-']
         });
 
-        const dc_web_access_sg = ec2.SecurityGroup.fromSecurityGroupId(this, 'security-group-stack-finder', 'sg-07ef0188e13238cb5');
+        const dc_web_access_sg = ec2.SecurityGroup.fromSecurityGroupId(this, 'security-group-stack-finder', 'sg-');
 
-        const role = Role.fromRoleArn(this, 'TaskRoleArn', 'arn:aws:iam::026180904564:role/ecsTaskExecutionRole', {
+        const role = Role.fromRoleArn(this, 'TaskRoleArn', 'arn:aws:iam::012345678901:role/ecsTaskExecutionRole', {
             mutable: false
         });
 
         
        const lb = elb2.ApplicationLoadBalancer.fromApplicationLoadBalancerAttributes(
             this, 'load-balancer-stack-finder', {
-                securityGroupId: 'sg-07ef0188e13238cb5',
-                loadBalancerArn: 'arn:aws:elasticloadbalancing:us-west-2:026180904564:loadbalancer/app/elb-ardis-dc-n25-dev/4ecdb540b435f460',
-                loadBalancerDnsName: 'internal-elb-ardis-dc-n25-dev-1898410798.us-west-2.elb.amazonaws.com'
+                securityGroupId: 'sg-',
+                loadBalancerArn: 'arn:aws:elasticloadbalancing:us-west-2:012345678901:loadbalancer/app/elb-',
+                loadBalancerDnsName: '.us-west-2.elb.amazonaws.com'
             }
         );
 
         const cluster = ecs.Cluster.fromClusterAttributes(this, 'cluster-stack-finder', {
-            clusterName: 'ardis-n25-dc',
+            clusterName: 'default-cluster',
             vpc,
             securityGroups: [dc_web_access_sg]
         });
@@ -85,7 +85,7 @@ export class WebService extends Construct {
 
         const listen = elb2.ApplicationListener.fromApplicationListenerAttributes(
             this, 'application-listener-stack-finder', {
-                listenerArn: 'arn:aws:elasticloadbalancing:us-west-2:026180904564:listener/app/elb-ardis-dc-n25-dev/4ecdb540b435f460/fe37cd8c5d8f2df3',
+                listenerArn: 'arn:aws:elasticloadbalancing:us-west-2:012345678901:listener/app/elb-',
                 securityGroup: dc_web_access_sg
             }
         );
@@ -106,7 +106,7 @@ export class WebService extends Construct {
         listen.addTargetGroups(
             'targetGroups-stack-finder', {
                 priority: 1600,
-                hostHeader: 'ardis-dev.pnl.gov',
+                hostHeader: 'domain.com',
                 pathPattern: '/stacks*',
                 targetGroups: [healthTarget]
             }
@@ -115,7 +115,7 @@ export class WebService extends Construct {
         
 
         var url = "https://ardis-dev.pnl.gov/stacks";
-        new CfnOutput(this, "BambooLink", {exportName: "BambooLink", value: "https://ci.pnnl.gov/bamboo/browse/"+String(process.env.bamboo_planKey)});
+        new CfnOutput(this, "BambooLink", {exportName: "BambooLink", value: "https://your.domain.com/bamboo/browse/"+String(process.env.bamboo_planKey)});
         new CfnOutput(this, 'LoadBalancerDNS', { value: lb.loadBalancerDnsName });
         new CfnOutput(this, 'MessageEndpoint', { exportName: 'message-endpoint-stack-finder', value:  url });
         new CfnOutput(this, 'webServiceName', { exportName: 'webservice-name-stack-finder', value: webService.serviceName });
